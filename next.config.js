c676621030f1de015595f627e1e8073d22f872fb
Matthias Oberholzer <matthias.m.oberholzer@gmail.com>
2022-01-22 15:13:17 +0100

    added image domain

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    domains: ["localhost"],
+    domains: ["localhost", "wild-code-challange.mot.codes"],
   },
 
   webpack: (config) => {
