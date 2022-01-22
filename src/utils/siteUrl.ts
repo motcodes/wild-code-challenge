af246af2a525441a976905f556d663f0c22e968c
Matthias Oberholzer <matthias.m.oberholzer@gmail.com>
2022-01-22 15:24:20 +0100

    fixed env var

--- src/utils/siteUrl.ts
+++ src/utils/siteUrl.ts
@@ -1,7 +1,4 @@
 export const siteUrl = (slug: string): string => {
-  const url =
-    process.env.NODE_ENV === "production"
-      ? process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000"
-      : "http://localhost:3000";
+  const url = process.env.NODE_ENV === "production" ? process.env.NEXT_PUBLIC_SITE_URL || "/" : "http://localhost:3000";
   return `${url}/${slug || ""}`;
 };
